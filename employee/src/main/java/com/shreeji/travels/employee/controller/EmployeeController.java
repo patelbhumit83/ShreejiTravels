adeb726cd207a4d411e92ae5a3f19125551b24c1
Bhumit Patel <patelbhumit83@gmail.com>
2022-07-22 15:14:40 -0400

    Delete API added to delete record

--- employee/src/main/java/com/shreeji/travels/employee/controller/EmployeeController.java
+++ employee/src/main/java/com/shreeji/travels/employee/controller/EmployeeController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -52,5 +54,16 @@ public class EmployeeController {
         Employee updatedEmployee = employeeRepository.save(employee);
 
         return ResponseEntity.ok(updatedEmployee);
+    }
+
+    //delete Employee Rest API
+    @DeleteMapping("employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFound("Employee Not Exist with id :- " + id ));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("deleted", true);
+        return ResponseEntity.ok(map);
     }
 }
