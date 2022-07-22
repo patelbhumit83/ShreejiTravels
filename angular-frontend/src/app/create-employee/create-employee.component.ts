efc6f3952165d89dc7e2c4d73f853c0f8c1bd800
Bhumit Patel <patelbhumit83@gmail.com>
2022-07-22 12:04:19 -0400

    backend create-employee api connected

--- angular-frontend/src/app/create-employee/create-employee.component.ts
+++ angular-frontend/src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee'
+import { EmployeeService} from '../employee.service'
+import { Router} from '@angular/router'
 
 @Component({
   selector: 'app-create-employee',
@@ -9,12 +11,25 @@ import { Employee } from '../employee'
 export class CreateEmployeeComponent implements OnInit {
 
   employee : Employee = new Employee();
-  constructor() { }
+  constructor(private employeeService:EmployeeService, private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  saveEmployee() {
+    this.employeeService.createEmployee(this.employee).subscribe(data => {
+      console.log(data);
+      this.goToEmoployeeList();
+    },
+    error => console.log(error));
+  }
+
+  goToEmoployeeList(){
+    this.router.navigate(['/employees']);
+  }
+
   onSubmit() {
+    this.saveEmployee();
   }
 
 }
